eb8b2cb05a1adb8f09ea3a93821f8bfd93964478
ChanAlice <chan_bing_yuen_alice@student.smc.edu>
2014-05-07 15:50:01 -0700

    Update HintPeg.java
    same as DecodingBoard, unable to upload the whole folder

--- src/moduleFolder/HintPeg.java
+++ src/moduleFolder/HintPeg.java
@@ -1,5 +1,35 @@
 package moduleFolder;
 
-public class HintPeg extends Peg {
+/*
+ * Description: This sets/gets the key peg color
+ * 05/05: first edited -- AC
+ * 
+ */
 
+import java.awt.Color;
+
+// inherits from Peg class for getPegColor
+// and setPegColor
+public class HintPeg extends Peg {
+    private Color pegColor;
+    
+    public HintPeg() {};
+    
+    public HintPeg(Color c) {
+    	super(c);
+    }
+    
+    // hint peg: only black and white
+    // black: correct in both color and position
+    // white: correct in color but wrong position
+    public Color decodePegColor(int i) {
+        switch (i) {
+            case (1): pegColor = Color.BLACK; break;
+            case (2): pegColor = Color.WHITE; break;
+        } 
+        
+        return pegColor;
+    }
+    
 }
+
