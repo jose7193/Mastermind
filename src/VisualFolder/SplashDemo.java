fde601e26c6400f03a5704872e14fda9f6bad6dc
anne_louise <annelouise@10.101.26.24>
2014-05-31 12:21:32 -0700

    Splash Demo No changes made

--- src/VisualFolder/SplashDemo.java
+++ src/VisualFolder/SplashDemo.java
@@ -71,6 +71,7 @@ public class SplashDemo extends JFrame implements ActionListener {
             }
         }
         splash.close();
+        
         setVisible(true);
         toFront();
     }
